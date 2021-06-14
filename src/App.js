7e5eef9fab50fdc24a466689a04ee4d0e7ee1fb7
Corey Tu <coreytu@gmail.com>
2021-06-15 02:27:30 +0800

    changes for updating amplify init and app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import {AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+
+
+Amplify.configure(awsconfig);
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <AmplifySignOut />
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Tian's Shopee Store ERP </h1>
       </header>
@@ -13,4 +22,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
